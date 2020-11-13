e6da3c7849b4aa51c31d466dd436bbf526aeb2f6
Filip Olsen <filip.kenya@gmail.com>
2020-11-13 13:05:10 +0100

    d3

--- Bank/bank.py
+++ Bank/bank.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, Header, HTTPException
 from pydantic import BaseModel
 import uvicorn
 
+import requests
 from datetime import datetime
 import sqlite3
 
@@ -10,7 +11,6 @@ class BankUser(BaseModel):
 	Userid: int
 
 class Loan(BaseModel):
-	id : int
 	BankUserid: int
 	Amount : int
 
@@ -92,16 +92,19 @@ async def read_account(account_id: id):
 
 # Bank Functionality
 class Deposit(BaseModel):
-	amount: int
-	bankuserid: int
+	BankUserId: int
+	Amount: int
 
 @app.post("/add-deposit", status_code=200)
 async def deposit_amount(deposit: Deposit):
-	if deposit.amount == null || deposit.amount < 0:
+	if deposit.Amount == null || deposit.Amount < 0:
         raise HTTPException(status_code=422, detail="Deposit needs to be greater than 0")
-	interest_added = deposit.amount	#CALL AZURE FUNCTION SOMEHOW
+	interest_added = deposit.Amount	#CALL AZURE FUNCTION SOMEHOW
+
+	#response = requests.get("localhost:7073/Interest_Rate")
+
 	query = """INSERT INTO Deposit (BankUserId, Amount) VALUES (?,?)"""
-	db.execute(query, [deposit.bankuserid, interest_added])
+	db.execute(query, [deposit.BankUserId, interest_added])
 	db.commit()
 
 @app.get("/list-deposits/{bankuserid}", status_code=200)
@@ -116,19 +119,16 @@ async def list_deposits(bui: bankuserid):
 
 	return deposits
 
-class Loan(BaseModel):
-	bankuserid: int
-	loanamount: int
-
 @app.post("/create-loan", status_code=200)
 async def create_loan(loan: Loan):
 	query = """SELECT Amount FROM Account WHERE BankUserId = '?';"""
-	c = db.execute(query, [loan.bankuserid])
+	c = db.execute(query, [loan.BankUserId])
 	db.commit()
 
 	account = c.fetchone()
 	
 	return_code = 0#CALL AZURE FUNCTION SOMEHOW
+	#response = requests.get("localhost:7073/Loan_Algorithm")
 
 	if return_code == 200:
 		query2 = """INSERT INTO Loan () VALUES ()"""
@@ -166,6 +166,36 @@ async def pay_loan(uid: bankuserid):
 
 		db.commit()
 
-@app.get("/list-loans")
+@app.get("/list-loans", status_code=200)
 async def list_loans():
+	#Read all loans
+	query = 'SELECT * FROM Loan'
+	db.execute(query)
+	select = db.commit()
+
+	loans = []
+	for row in select:
+		loans.append({'BankUserId':row[1], 'CreatedAt':row[2], 'ModifiedAt':row[3], 'Amount':row[4]})
+
+	return loans
+
+class Withdraw(BaseModel):
+	UserId: int
+	Amount : int
+	## The body of that request should contain an amount and a UserId(Not BankUserId, not SkatUserId)
+    ## Subtract (if possible) the amount from that users account. Throw an error otherwise.
+@app.get("withdrawal-money", status_code=200)
+async def withdraw-money(withdrawModel: Withdraw):
+	query = 'SELECT Amount FROM Account WHERE Id = ?'
+	selectAccountAmount = db.execute(query, withdrawModel.UserId)
+
+	íf (withdrawModel.Amount > selectAccountAmount.fetchone()['Amount'])
+	{
+		raise HTTPException(status_code=422, detail="withdraw amount is greater than account amount")
+	}
+	else
+	{
+		query2 = 'UPDATE Account SET amount = ? WHERE Id = ?'
+		db.execute(query2, [selectAccountAmount.fetchone()['Amount'] - withdrawModel.Amount, withdrawModel.UserId])
+	}	
 	
